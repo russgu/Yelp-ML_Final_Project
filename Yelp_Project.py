438fc52473a216c46f3c1421722a1fd88f8e65b5
EmZhu <jielei.emma.zhu@gmail.com>
2016-04-05 20:08:10 -0400

    fixed bug
    All reviews were dumped in one line, which made impossible to open the file.

--- Yelp_Project.py
+++ Yelp_Project.py
@@ -1,37 +1,35 @@
 import json
 
 #======== BUSINESS CRITERIA ==========#
-geography = 'city'
-geo_name = 'Las Vegas'
-business_type = 'Restaurants'
+geography = ’city’
+geo_name = ’Las Vegas’
+business_type = ’Restaurants’ 
 #=====================================#
-
-
 
 res_b_id = [] # qualified businesses (represented by their business id)
 
 # finding all qualified restaurants
-with open('yelp_academic_dataset_business.json','r') as json_file:
+with open(’yelp_academic_dataset_business.json’,’r’) as json_file:
     for line in json_file:
         b_temp = json.loads(line)
-        if b_temp[geography] == geo_name and business_type in b_temp['categories']:
-            res_b_id.append(b_temp['business_id'])
+        if b_temp[geography] == geo_name and business_type in b_temp[’categories’]:
+            res_b_id.append(b_temp[’business_id’])
             
+            
+            
+            
+review_count = 0 # customer reviews on selected businesses
 
-
-res_review = [] # customer reviews on selected businesses
-
-with open('yelp_academic_dataset_review.json','r') as json_file:
+with open('yelp_academic_dataset_review.json','r') as json_file,\
+open('(yelp)selected_reviews.json','w') as file:
     for line in json_file:
         review_temp = json.loads(line)
         if review_temp['business_id'] in res_b_id:
-            res_review.append(review_temp['text'])
-            
-            
-            
-with open('(yelp)selected_revews.json','w') as file:
-    json.dumps(res_review, file)
-    
-    
+            json.dump({review_temp['business_id'] : review_temp['text']}, file)
+            file.write('\n')
+            review_count += 1
+                     
+                     
+                     
 print('There are ', len(res_b_id), ' restaurants in ', geo_name)
-print('There are ', len(res_review), ' reviews on selected businesses')
+print('There are ', len(review_count), ' reviews on selected businesses')
