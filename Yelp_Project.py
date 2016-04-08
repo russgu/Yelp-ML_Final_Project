f1d17f420b65ed143d1f333ffa8730c8e3f97756
erikalage <erika@compstak.com>
2016-04-07 23:01:38 -0400

    Modify reviews to include star rating, uid

--- Yelp_Project.py
+++ Yelp_Project.py
@@ -1,19 +1,19 @@
 import json
 
 #======== BUSINESS CRITERIA ==========#
-geography = ’city’
-geo_name = ’Las Vegas’
-business_type = ’Restaurants’ 
+geography = 'city'
+geo_name = 'Las Vegas'
+business_type = 'Restaurants' 
 #=====================================#
 
 res_b_id = [] # qualified businesses (represented by their business id)
 
 # finding all qualified restaurants
-with open(’yelp_academic_dataset_business.json’,’r’) as json_file:
+with open('yelp_academic_dataset_business.json','r') as json_file:
     for line in json_file:
         b_temp = json.loads(line)
-        if b_temp[geography] == geo_name and business_type in b_temp[’categories’]:
-            res_b_id.append(b_temp[’business_id’])
+        if b_temp[geography] == geo_name and business_type in b_temp['categories']:
+            res_b_id.append(b_temp['business_id'])
             
             
             
@@ -25,11 +25,11 @@ open('(yelp)selected_reviews.json','w') as file:
     for line in json_file:
         review_temp = json.loads(line)
         if review_temp['business_id'] in res_b_id:
-            json.dump({review_temp['business_id'] : review_temp['text']}, file)
+            json.dump({'business_id':review_temp['business_id'],  'user_id': review_temp['user_id'], 'stars':review_temp['stars'], 'text':review_temp['text']}, file)
             file.write('\n')
             review_count += 1
                      
                      
                      
-print('There are ', len(res_b_id), ' restaurants in ', geo_name)
-print('There are ', len(review_count), ' reviews on selected businesses')
+print('There are ', str(len(res_b_id)), ' restaurants in ', geo_name)
+print('There are ', str(len(review_count)), ' reviews on selected businesses')
