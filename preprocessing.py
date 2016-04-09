38930fcf2a5f5b0e4447d0ecfe811037b7e66af6
erikalage <erika@compstak.com>
2016-04-08 20:31:44 -0400

    Changes to preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,30 +1,39 @@
 import json, re
 from collections import Counter
+from nltk.stem.snowball import SnowballStemmer
 
 def features_from_reviews():
+    stemmer = SnowballStemmer("english", ignore_stopwords=True)
+    
     f = open('(yelp)selected_reviews.json', 'r')
     reviews = []
     stars = []
     words = []
-##    i = 0
+    i = 0
     for line in f:
         line = json.loads(line)
         text = line['text'].lower().replace("'", "")
         text = re.sub("[^a-z]", " ", text)
         text = set(text.split())
         for w in text:
+            w = stemmer.stem(w)
             words.append(w)
         text = " ".join(text)
         reviews.append(text)
         stars.append(line['stars'])
 
-##        i += 1
-##        if i == 10:
-##            break
+        i += 1
+        if i == 500:
+            break
     f.close()
-  
+
     word_counts = Counter(words)
+    print len(word_counts)
     words = [k for k, v in word_counts.items() if v >= 50]
+
+    print len(words)
+    print len(reviews)
+    
     f = open('dictionary_0.txt', 'w')
     for word in words:
         f.write(word + "\n")
