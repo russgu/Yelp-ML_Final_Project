a942d9206799a755f5cc002010852db3bbbe8cfd
Zi_Gu <guzi@172-16-209-204.DYNAPOOL.NYU.EDU>
2016-04-30 13:43:54 -0400

    bigram into text now writes to a new file (yelp)selected_reviews_new.json

--- bigrams_into_text.py
+++ bigrams_into_text.py
@@ -1,7 +1,9 @@
 import json 
 
 with open ("(yelp)selected_reviews.json","r") as file,\
-open("Anchor-Bigrams.txt","r") as anchor_file:
+open("Anchor-Bigrams.txt","r") as anchor_file,\
+open("(yelp)selected_reviews_new.json","w") as new_file:
+
 	anchors=[]
 	for line in anchor_file:
 		line=line.strip('\n');
@@ -15,6 +17,7 @@ open("Anchor-Bigrams.txt","r") as anchor_file:
 			if word in review: #searching bigrams in review text
 				replace=word.replace(" ", "_")
 				review=review.replace(" "+word+""," "+replace+"");
-
 		data["text"] = review;
+		json.dump({'business_id':data['business_id'],  'user_id': data['user_id'], 'stars':data['stars'], 'text':data['text']}, new_file, ensure_ascii=True)
+		new_file.write('\n')
    
